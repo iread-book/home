c479858b04a55c47a025d37c2d952f034defaef8
iread-book <139600106+iread-book@users.noreply.github.com>
2023-09-08 13:04:43 +0900

    Add files via upload

--- js/m.js
+++ js/m.js
@@ -3,3 +3,13 @@ function gohome() {
    $read = loc.protocol + "//" + loc.host + loc.pathname;
    window.location = $read;
    }
+function newContent(pageId) {
+    if(pageId==='1'){
+      document.getElementById("pageContent").innerHTML = "comming soon";
+    }else{
+
+      var customTextElement = document.getElementById('pageContent');
+        document.getElementById("pageContent").innerHTML = '<iframe src="' + pageId + '.html" scrollable="no" style="height:300px"></iframe>';
+    } 
+    
+  }
